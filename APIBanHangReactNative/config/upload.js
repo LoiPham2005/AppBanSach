c5359efe873f2639144f3cd0c7110929933c86a9
đức lợi phạm <phamducloi919@gmail.com>
2025-01-22 08:53:37 +0700

    addAPI

---
+++ APIBanHangReactNative/config/upload.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname.replace(/\s+/g, '-')}`)
+    }
+});
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(new Error('Not an image! Please upload only images.'), false);
+        }
+    },
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+});
+
+module.exports = upload;
---
+++ APIBanHangReactNative/models/model_products.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const products = new Schema({
+    images: [{ type: String, required: true }], // Array of images
+    title: { type: String, required: true },
+    publishing_house: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    stock_quantity: { type: Number, required: true },
+    id_category: { type: Schema.Types.ObjectId, ref: 'category', required: true },
+    status: { 
+        type: String, 
+        enum: ['active', 'out of stock', 'importing goods', 'stop selling'], 
+        default: 'active' 
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('products', products);
